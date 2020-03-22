425a9de2e48877a2f7ec27ebd923998b1265145f
VadimZ75 <61960809+VadimZ75@users.noreply.github.com>
2020-03-22 16:07:09 +0500

    Create lesson11.py

---
+++ lesson11.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import time
+import math
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    browser.find_element_by_class_name('btn-primary').click()
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+    ans = browser.find_element_by_id('answer')
+    ans.send_keys(y)
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    time.sleep(10)
+    browser.quit()
