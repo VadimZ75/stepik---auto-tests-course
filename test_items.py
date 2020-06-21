12810697d19b49f93a2bab720914d0db58a577b6
VadimZ75 <61960809+VadimZ75@users.noreply.github.com>
2020-06-21 07:40:04 +0500

    change file

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/."
 
-def test_guest_should_see_login_link_pass(browser):
+def test_guest_should_see_basket_form(browser):
     browser.get(link)
     browser.implicitly_wait(30)
     rezult = browser.find_element_by_css_selector("#add_to_basket_form")
